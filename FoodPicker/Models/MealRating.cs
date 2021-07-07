f4eb043299ba2abf42f1f9378c445159e3401396
Evan Lihou <e@evandoes.dev>
2021-07-06 20:07:54 -0400

    Let's try that again

--- FoodPicker/Models/MealRating.cs
+++ FoodPicker/Models/MealRating.cs
@@ -6,11 +6,12 @@ namespace FoodPicker.Models
 {
     public class MealRating
     {
+        public const int MaxRating = 4;
         public int Id { get; set; }
         public int MealId { get; set; }
         [ForeignKey(nameof(MealId))]
         public Meal Meal { get; set; }
-        [Range(1,4)]
+        [Range(1,MaxRating)]
         [Display(Name = "Rating")]
         public int Rating { get; set; }
         [Display(Name = "Comment")]
